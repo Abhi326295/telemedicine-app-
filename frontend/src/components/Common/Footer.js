dae01f5b5466ae44b1ad895a01d70fb8e3799745
Abhi326295 <abhinandan3262@gmail.com>
2024-06-24 12:50:20 +0530

    Create Footer.js

---
+++ frontend/src/components/Common/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Common/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 TelemedicineApp. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
