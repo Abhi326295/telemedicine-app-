785d6892789266d957141aeed02da375147365ae
Abhi326295 <abhinandan3262@gmail.com>
2024-06-24 12:38:11 +0530

    Create usermodel.js

---
+++ backend/models/usermodel.js
@@ -0,0 +1,12 @@
+// backend/models/user.model.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['patient', 'doctor', 'admin'], default: 'patient' }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
