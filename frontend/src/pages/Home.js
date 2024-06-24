e1f13679f7a638c1a8a7c4944298a72c2163297d
Abhi326295 <abhinandan3262@gmail.com>
2024-06-24 12:51:13 +0530

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to TelemedicineApp</h1>
+      <p>Your health, our priority. Book an appointment with a doctor now.</p>
+    </div>
+  );
+};
+
+export default Home;
